c3d8d7269c4441b6b05654a4d38981cc99028876
releaser <herminiohenriques@gmail.com>
2023-11-30 03:52:49 +0000

    dynamic production address fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,16 +1,20 @@
-async function getData() {
-  /*   const res = await fetch("http://localhost:3000/api/posts");
+import getDomain from "../lib/getDomain";
 
+async function getData() {
+  const domain = getDomain();
+  const res = await fetch(`${domain}/api/posts`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
-  return res.json(); */
-  return { items: [] };
+  return res.json();
+  //return { items: [] };
 }
 
 export default async function BlogPage() {
   const data = await getData();
   const items = data && data.items ? [...data.items] : [];
+  console.log(items);
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
   return (
     <main>
       <h1>Hello from blogpage</h1>
