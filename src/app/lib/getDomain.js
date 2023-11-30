83741914b0918bc7d00d44cbc206aca3f258431c
releaser <herminiohenriques@gmail.com>
2023-11-30 05:25:27 +0000

    Updated getDomain function

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,8 +1,8 @@
 export default function getDomain() {
   const protocol =
     process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http";
-  const domain = process.env.PUBLIC_URL
-    ? process.env.PUBLIC_URL
+  const domain = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
     : "localhost:3000";
   return `${protocol}://${domain}`;
 }
