60d718dce8d1ca795ca8f0ba8f9335c5c54d6690
releaser <herminiohenriques@gmail.com>
2023-11-30 04:14:31 +0000

    update blog/page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,12 +3,9 @@ import getDomain from "../lib/getDomain";
 async function getData() {
   // 1 endpoint - API?
   const domain = getDomain();
+  const endpoint = `${domain}/api/posts`; // -> third party api request??
   // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
-  const res = await fetch(
-    `${domain}/api/posts`,
-    { cache: "no-store" },
-    { next: { revalidate: 10 } }
-  ); // HTTP GET
+  const res = await fetch(endpoint, { cache: "no-store" }); // HTTP GET
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
