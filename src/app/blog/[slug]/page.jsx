c29d2c5837017b745da510daa9d244c37a98c113
releaser <herminiohenriques@gmail.com>
2023-11-30 03:06:27 +0000

    initial setup, crud and slug endpoints

---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,8 @@
+export default function BlogPostDetail(params) {
+  console.log(params);
+  return (
+    <main>
+      <h1>Hello from Blog Detail Page</h1>
+    </main>
+  );
+}
---
+++ src/app/blog/page.jsx
@@ -0,0 +1,23 @@
+async function getData() {
+  const res = await fetch("http://localhost:3000/api/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function BlogPage() {
+  const data = await getData();
+  const items = data && data.items ? [...data.items] : [];
+  return (
+    <main>
+      <h1>Hello from blogpage</h1>
+      <p>Posts:</p>
+      {items &&
+        items.map((item, idx) => {
+          return <li key={`post-${idx}`}>{item.title}</li>;
+        })}
+    </main>
+  );
+}
